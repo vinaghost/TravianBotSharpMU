ae2e38960e251ce1035a1f31f2fb6bed1a3d81ee
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-07-06 23:03:18 +0700

    add contextualhelp status to accountinfo model

---
+++ MainCore/Migrations/202307062300_ContextualHelp.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace MainCore.Migrations
+{
+    [Migration(202307062300)]
+    public class ContextualHelp : Migration
+    {
+        public override void Down()
+        {
+            Execute.Sql("ALTER TABLE 'AccountsInfo' DROP COLUMN 'IsContextualHelpDisabled';");
+        }
+
+        public override void Up()
+        {
+            Alter.Table("AccountsInfo")
+                .AddColumn("IsContextualHelpDisabled").AsBoolean().WithDefaultValue(false);
+        }
+    }
+}
